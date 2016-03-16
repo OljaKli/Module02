4e86efd573f49a13fdb4ce676d2512da96a3c5ad
OljaKli <olja.klisho@gmail.com>
2016-03-16 21:39:11 +0300

    *6th task started

---
+++ src/olja/klisho/task01_/Pen.java
@@ -0,0 +1,44 @@
+package olja.klisho.task01_;
+
+import java.util.Objects;
+
+/**
+ * Created by Ola-Mola on 16/03/16.
+ */
+public class Pen {
+
+    private String color;
+    private String label;
+    private Double Price;
+
+    public Pen(String color, String label, Double price) {
+        this.color = color;
+        this.label = label;
+        this.Price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pen pen = (Pen) o;
+        return Objects.equals(color, pen.color) &&
+                Objects.equals(label, pen.label) &&
+                Objects.equals(Price, pen.Price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, label, Price);
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("olja.klisho.task01_.Pen{");
+        sb.append("color='").append(color).append('\'');
+        sb.append(", label='").append(label).append('\'');
+        sb.append(", price=").append(Price);
+        sb.append('}');
+        return sb.toString();
+    }
+}
--- src/olja/klisho/task06/NuclearBoard.java
+++ src/olja/klisho/task06/NuclearBoard.java
@@ -1,4 +1,4 @@
-package olja.klisho.task03;
+package olja.klisho.task06;
 
 /**
  * Created by Ola-Mola on 16/03/16.
